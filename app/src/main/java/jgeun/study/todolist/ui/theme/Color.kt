8d98c482cac1df30fa86f560732dfadc762dccf8
JGeun <pjk8926@gmail.com>
2023-07-05 01:15:17 +0900

    초기 설정

---
+++ app/src/main/java/jgeun/study/todolist/ui/theme/Color.kt
@@ -0,0 +1,25 @@
+package jgeun.study.todolist.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val Purple80 = Color(0xFFD0BCFF)
+val PurpleGrey80 = Color(0xFFCCC2DC)
+val Pink80 = Color(0xFFEFB8C8)
+
+val Purple40 = Color(0xFF6650a4)
+val PurpleGrey40 = Color(0xFF625b71)
+val Pink40 = Color(0xFF7D5260)
+
+val TextMainColor = Color(0xFF000000)
+val MsgColor = Color(0xFF767676)
+val HintColor = Color(0xFF777777)
+val ProgressAreaStrokeColor = Color(0xFFB1B1B1)
+val ProgressBarDefault = Color(0xFFD9D9D9)
+val ProgressBarCompleted = Color(0xFF5093E1)
+val AddBtnBgColor = Color(0xFF637CFF)
+val DialogBtnStrokeColor = Color(0xFF646464)
+val Purple = Color(0xFFB799E9)
+val Green = Color(0xFF40D67C)
+val Blue = Color(0xFF648EFA)
+val Sky = Color(0xFF8DDDF6)
+val Emerald = Color(0xFF60D2BE)
